a3dbcbce9832a3ba071b80294f97f978bbaa3cb7
Kevin Smith <kevintylersmith1@gmail.com>
2016-03-30 16:34:57 -0500

    iteration-1
    first question in game

---
+++ choose-adventure-game.js
@@ -0,0 +1,16 @@
+confirm ("Are you ready to play?")
+
+var age= prompt("what's your age?")
+
+if (age < 13) {debug("you are wise enough to do well")}
+
+else
+	{debug("you are too old to do well. Good luck")}
+
+debug("You slowly walk through the park on your way home after a long days work when you see a homeless man asleep on the bench. His blanket has fallen off and he's shivering.")
+
+var userAnswerOne = prompt("Do you cover the homeless man?")
+
+if (userAnswerOne === "yes") {debug("The homeless man wakes up and infuriatingly curses at you, telling you to leave him alone.")}
+
+else {debug("You turn your head to the right to keep from seeing the man. You look down and find a 20 dollar bill! Nice!")}
